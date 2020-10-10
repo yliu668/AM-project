c5e7d8208af9e461b5a576e23afce811c73aa477
YL <40039773+yliu668@users.noreply.github.com>
2020-10-10 16:39:18 -0400

    Update

--- AM_power_analysis.R
+++ AM_power_analysis.R
@@ -3,9 +3,8 @@ library(BUCSS)
 # alpha.prior can be set to 1 to indicate no publication bias.
 
 # McBeath & Shepard (1989)
-ss.power.wa.general(313, 10, 3, alpha.prior =0.9, 
-                     alpha.planned = 0.05, assurance = 0.5,
-                     power = 0.8, step = 0.001) # n=3 per group
+ss.power.spa.general(313, 10, 3, 2, effect = "within.only", alpha.prior = 0.05, 
+                     alpha.planned = 0.05, assurance = 0.5, power = 0.8, step = 0.001) # n=2 per group
 
 # Yantis & Nakayama (1998)
 ss.power.dt(4.35, 10, alpha.prior = 0.8, 
